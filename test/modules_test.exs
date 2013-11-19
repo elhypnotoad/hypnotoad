0444f8fb2e58533de12ce85969c7554627a37cf9
Yurii Rashkovskii <yrashk@gmail.com>
2013-11-19 07:33:25 -0800

    Fix tests

--- test/modules_test.exs
+++ test/modules_test.exs
@@ -9,32 +9,41 @@ defmodule Hypnotoad.Test.Modules do
 
   test "requirements can be passed as expressions" do
   	# the following is always valid because :erlang.now always returns unique values
-    assert Enum.find(RequiresExpr.requirements, fn({Some, _}) -> true; (_) -> false end) != 
-           Enum.find(RequiresExpr.requirements, fn({Some, _}) -> true; (_) -> false end)
+    assert Enum.find(RequiresExpr.requirements, fn({Some, _, _}) -> true; (_) -> false end) != 
+           Enum.find(RequiresExpr.requirements, fn({Some, _, _}) -> true; (_) -> false end)
   end
 
   defmodule DefaultAttributes do
   	use Hypnotoad, default_attributes: [version: "0.6.6"]
 
    	requires Package, name: "lxc-docker-#{attributes[:version]}"
-    requires SomePackage do
-      if attributes[:extra_package] do
-        [name: "my-package"]
-      end
-    end
+    requires SomePackage, [name: "my-package"], only: attributes[:extra_package] != nil
   end
 
   test "attributes can be passed to requirements" do
-    assert {Package, [name: "lxc-docker-0.6.7"]} = Enum.find(DefaultAttributes.requirements(version: "0.6.7"), fn({Package, _}) -> true; (_) -> false end)
+    assert {Package, [name: "lxc-docker-0.6.7"], nil} = Enum.find(DefaultAttributes.requirements(version: "0.6.7"), fn({Package, _, _}) -> true; (_) -> false end)
   end
 
   test "attributes can be set to defaults" do
-    assert {Package, [name: "lxc-docker-0.6.6"]} = Enum.find(DefaultAttributes.requirements, fn({Package, _}) -> true; (_) -> false end)
+    assert {Package, [name: "lxc-docker-0.6.6"], nil} = Enum.find(DefaultAttributes.requirements, fn({Package, _, _}) -> true; (_) -> false end)
   end
 
   test "requirements can be skipped depending on attributes" do
-    assert {SomePackage, [name: "my-package"]} = Enum.find(DefaultAttributes.requirements(extra_package: true), fn({SomePackage, _}) -> true; (_) -> false end)    
-    assert nil = Enum.find(DefaultAttributes.requirements, fn({SomePackage, _}) -> true; (_) -> false end)    
+    assert {SomePackage, [name: "my-package"], nil} = Enum.find(DefaultAttributes.requirements(extra_package: true), fn({SomePackage, _, _}) -> true; (_) -> false end)    
+    assert nil = Enum.find(DefaultAttributes.requirements, fn({SomePackage, _, _}) -> true; (_) -> false end)    
+  end
+
+  defmodule NestedRequires do
+    use Hypnotoad
+
+    requires Some do
+      requires Another, test: 1
+    end
+  end
+
+  test "requirements can be nested" do
+     assert {Some, [], nil} = Enum.find(NestedRequires.requirements, fn({Some, _, _}) -> true; (_) -> false end)
+     assert {Another, [test: 1], {Some, []}} = Enum.find(NestedRequires.requirements, fn({Another, _, _}) -> true; (_) -> false end)
   end
 
 end
